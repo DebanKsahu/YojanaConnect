5d3f76407fdee3c74caa5e6151088a9fc5e33579
Deban Kumar Sahu <sahudebankumar@gmail.com>
2025-06-15 21:33:06 +0530

    Implemented and tested login and signup feature

--- Database/Engine/__init__.py
+++ Database/Engine/__init__.py
@@ -1,6 +1,7 @@
 import os
 from sqlmodel import create_engine,SQLModel
 from dotenv import load_dotenv
+from typing import cast
 
 load_dotenv()
 USERNAME = os.getenv("USERNAME")
@@ -8,5 +9,6 @@ PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}?sslmode=require"
+TESTING_DATABASE_URL = cast(str,os.getenv("TESTING_DATABASE_URL"))
 
-engine = create_engine(url=DATABASE_URL,echo=True)
+engine = create_engine(url=TESTING_DATABASE_URL,echo=True)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from contextlib import asynccontextmanager
+from Database.Engine import engine
+from Auth.auth import auth_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router=auth_router, tags=["Auth"])
